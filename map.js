79ecbf6861e1b691de3b8ddf17758f485a855920
Kelechi Obikaonu <kelechipauline@gmail.com>
2020-11-26 20:43:24 +0000

    this is a map function

---
+++ map.js
@@ -0,0 +1,10 @@
+const map = function (arr, callback) {
+  const results = [];
+  for(let item of arr) {
+    results.push(callback(item));
+  }
+  return results;
+};
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+console.log(results1);
