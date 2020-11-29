3319840959581b8504c107cf35f1fbcebecba5b5
Kelechi Obikaonu <kelechipauline@gmail.com>
2020-11-29 06:45:46 +0000

    test code

---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1]));
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
+console.log(middle([1, 2, 3]));
