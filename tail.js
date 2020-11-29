89a1ee228f7cda72883d2060deaaea863bde2cd6
Kelechi Obikaonu <kelechipauline@gmail.com>
2020-11-29 05:47:46 +0000

    refactored tail

--- tail.js
+++ tail.js
@@ -1,17 +1,5 @@
 //This function implement the tail function.
 const tail = function(arr) {
-  const newArr = arr.slice(1);
-  console.log(newArr);
+  return arr.slice(1);
 };
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(String.fromCodePoint(0x2705) + `Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(String.fromCodePoint(0x274C) + `Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-//const result = tail(["Hello", "Lighthouse", "Labs"])
-//assertEqual(tail(result, ["Lighthouse", "Labs"] ), "Hello");//assertEqual("Lighthouse Labs", "Lighthouse Labs");
-const words = ["Yo Yo"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+module.exports = tail;
