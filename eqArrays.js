c5dce9d80008f64136ef58a82830def4d7c60147
Kelechi Obikaonu <kelechipauline@gmail.com>
2020-11-29 06:11:39 +0000

    refactored coded

--- eqArrays.js
+++ eqArrays.js
@@ -1,27 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(String.fromCodePoint(0x2705) + `Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(String.fromCodePoint(0x274C) + `Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(10, 1);
-const eqArrays = function(arr1, arr2) {
-  let trueOrFalse = '';
+const eqArrays = function(arr1,arr2) {
   if (arr1.length !== arr2.length) {
-    trueOrFalse = false;
+    return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      trueOrFalse = true;
-    } else trueOrFalse = false;
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
   }
-  return trueOrFalse;
+  return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+module.exports = eqArrays;
